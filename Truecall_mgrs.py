78d7b0c61f330a32b3dc2c9e442be82e0018be34
Zhe Sun <injure21@gmail.com>
2023-11-07 12:21:49 -0500

    workable version

--- Truecall_mgrs.py
+++ Truecall_mgrs.py
@@ -25,7 +25,7 @@ from operator import add
 from sklearn.neighbors import BallTree
 from math import radians, cos, sin, asin, sqrt
 
-def process_csv_files(date_range, file_path_pattern,partitionNum, hdfs = 'hdfs://njbbvmaspd11.nss.vzwnet.com:9000', dropduplicate = False): 
+def process_csv_files(date_range, file_path_pattern,partitionNum, hdfs = 'hdfs://njbbvmaspd11.nss.vzwnet.com:9000',sample_percentage = 1, dropduplicate = False): 
 
     """ 
     Reads CSV files from HDFS for the given date range and file path pattern and processes them.
@@ -42,7 +42,7 @@ def process_csv_files(date_range, file_path_pattern,partitionNum, hdfs = 'hdfs:/
     for d in date_range: 
         file_path = file_path_pattern.format(d) 
         try:
-            df_kpis = spark.read.option("recursiveFileLookup", "true").option("header", "true").csv(file_path).repartition(partitionNum)
+            df_kpis = spark.read.option("recursiveFileLookup", "true").option("header", "true").csv(file_path).sample(sample_percentage).repartition(partitionNum)
             if dropduplicate:
                 df_kpis = df_kpis.dropDuplicates()
             else:
@@ -129,7 +129,6 @@ def get_date_window(start_date, end_time = None,  days = 1, direction = "forward
         pass
     
     return date_list
-
 def convert_string_numerical(df, String_typeCols_List): 
     """ 
     This function takes a PySpark DataFrame and a list of column names specified in 'String_typeCols_List'. 
@@ -149,86 +148,100 @@ def convert_string_numerical(df, String_typeCols_List):
 
 if __name__ == "__main__":
     # the only input is the date which is used to generate 'date_range'
-    spark = SparkSession.builder.appName('Truecall mgrs').enableHiveSupport().getOrCreate()
+    spark = SparkSession.builder.appName('Truecall_mgrs').enableHiveSupport().getOrCreate()
     parser = argparse.ArgumentParser(description="Inputs for generating Post SNA Maintenance Script Trial")
-    # count total number of data
+
+    partitionNum = 12000
+    sample_perc = 0.1
+
+#---------------------------------------------------------------------------------
+    hdfs_title = 'hdfs://njbbvmaspd11.nss.vzwnet.com:9000'
+    Numerical_Column = ['call_duration','dl_mac_volume_bytes','dl_pdcp_volume_bytes','dl_tti_ue_scheduled','drb_mean_pdcp_dl_kbps','drb_mean_pdcp_ul_kbps','end_latitude','end_longitude','endc_downlink_bytes','endc_duration','endc_duration_rate','endc_sgnb_addition_attempts_count','endc_sgnb_addition_failures_count','endc_sgnb_drops_count','endc_uplink_bytes','pucch_sinr_db','pusch_sinr_db','rsrp_dbm','rsrq_db','n1_rsrp_dbm', 'n1_rsrq_db', 'n2_rsrp_dbm', 'n2_rsrq_db', 'n3_rsrp_dbm', 'n3_rsrq_db', 'mac_mean_dl_kbps', 'mac_mean_ul_kbps', 'nr_dl_sinr_db', 'nr_rsrp_dbm', 'nr_rsrq_db','rlc_pdu_dl_bytes_count','rlc_pdu_ul_bytes_count','ul_mac_volume_bytes','ul_sinr_db']
+    String_Column =['tac','current_cell_id','current_dl_earfcn','current_enodeb_id','current_nr_measurement_arfcn','current_nr_measurement_cell','current_nr_measurement_pci','indoor_outdoor_indicator_name','initial_cell_id','initial_dl_earfcn','initial_enodeb_id','nr_serving_arfcn','nr_serving_pci','nr_sgnb_x2_release_cause_code','service_type_name','serving_nr_cell_id','session_data_lost_flag','subscriber_id','userequipment_imeisv', 'vendor_name', 'userequipment_manufacturer_name', 'userequipment_model_name', 'submkt']
+    Date_Column = ['call_end_timestamp','call_start_timestamp','current_nr_rrc_report_time']
+#---------------------------------------------------------------------------------
+
+    today_str = datetime.now().strftime("%Y-%m-%d")
+    d_range = get_date_window(today_str, days = 9, direction = 'backward')
+    d_range = list( map(lambda x: x.replace("-",""),d_range) )
+
+    file_path_pattern = "/user/jennifer/truecall/TrueCall_VMB/UTC_date={}/"  
+    df_list = process_csv_files(d_range, file_path_pattern,partitionNum,sample_percentage=sample_perc)
+    # def process_csv_files(date_range, file_path_pattern,partitionNum, hdfs = 'hdfs://njbbvmaspd11.nss.vzwnet.com:9000', dropduplicate = False): 
+    df_trc_sampled = union_df_list(df_list)
+
+    # convert specific String column to Numerical column
+    df_trc_sampled = convert_string_numerical(df_trc_sampled,String_Column+Date_Column)
+
+    # filter Null lat-log
+    df_trc_sampled = df_trc_sampled.filter(col('end_latitude').isNotNull() ).filter(col('end_longitude').isNotNull() )
+
+    # Indoor-Outdoor
+    df_trc_sampled = df_trc_sampled.withColumn("env_tag", when(col("indoor_outdoor_indicator_name") == "Indoor", "Indoor").otherwise("Outdoor"))
+# Convert lat-long to mgrs---------------------------------------------------------------------------------
+
+    # user-defined function to create mgrs/gridid
+    import mgrs
+    m = mgrs.MGRS()
+
+    def convert_to_mgrs(latitude, longitude):
+        try:
+            mgrs_value = m.toMGRS(latitude, longitude)
+            return mgrs_value
+        except:
+            return None
+
+    mgrs_udf = udf(convert_to_mgrs, StringType() ) 
+    df_trc_mgrs = df_trc_sampled.withColumn('gridid', mgrs_udf(df_trc_sampled['end_latitude'],df_trc_sampled['end_longitude']))
+#---------------------------------------------------------------------------------
+    df_mgrs_feature = df_trc_mgrs.groupBy("gridid", "env_tag").agg(
+                    avg("rsrp_dbm").alias("rsrp_dbm"),
+                    avg("rsrq_db").alias("rsrq_db"),
+                    sum("rlc_pdu_dl_bytes_count").alias("sum_rlc_pdu_dl_bytes_count"),
+                    sum("rlc_pdu_ul_bytes_count").alias( "sum_rlc_pdu_ul_bytes_count"),
+                    sum("dl_mac_volume_bytes").alias( "dl_mac_volume_bytes"),\
+                    sum("ul_mac_volume_bytes").alias( "ul_mac_volume_bytes"),\
+                    avg("mac_mean_ul_kbps").alias( "mac_mean_ul_kbps"),\
+                    avg("mac_mean_dl_kbps").alias( "mac_mean_dl_kbps"),\
+                    avg("drb_mean_pdcp_dl_kbps").alias( "drb_mean_pdcp_dl_kbps"),\
+                    avg("drb_mean_pdcp_ul_kbps").alias( "drb_mean_pdcp_ul_kbps"),\
+                    sum("dl_pdcp_volume_bytes").alias( "dl_pdcp_volume_bytes"),\
+                    avg("pusch_sinr_db").alias( "pusch_sinr_db") )
+
+#---------------------------------------------------------------------------------
+    output_path = 'hdfs://njbbvmaspd11.nss.vzwnet.com:9000/user/ZheS/TrueCall/df_mgrs_feature.csv' 
+    print('flag finished')
+    df_mgrs_feature.coalesce(1000) .write.format("csv").option("header", "true")\
+                    .mode("overwrite")\
+                    .option("compression", "gzip")\
+                    .save(output_path)
+#---------------------------------------------------------------------------------
+
+
+    """    # count total number of data
     partitionNum = 12000
 
-    Numerical_Column = ['call_duration','dl_mac_volume_bytes','dl_pdcp_volume_bytes','dl_tti_ue_scheduled','drb_mean_pdcp_dl_kbps','drb_mean_pdcp_ul_kbps','end_latitude','end_longitude','endc_downlink_bytes','endc_duration','endc_duration_rate','endc_sgnb_addition_attempts_count','endc_sgnb_addition_failures_count','endc_sgnb_drops_count','endc_uplink_bytes','pucch_sinr_db','pusch_sinr_db','rsrp_dbm','rsrq_db','n1_rsrp_dbm', 'n1_rsrq_db', 'n2_rsrp_dbm', 'n2_rsrq_db', 'n3_rsrp_dbm', 'n3_rsrq_db', 'mac_mean_dl_kbps', 'mac_mean_ul_kbps', 'nr_dl_sinr_db', 'nr_rsrp_dbm', 'nr_rsrq_db','rlc_pdu_dl_bytes_count','rlc_pdu_ul_bytes_count','ul_mac_volume_bytes','ul_sinr_db']
+    #base_path1 = "hdfs://njbbvmaspd11.nss.vzwnet.com:9000/user/jennifer/truecall/TrueCall_VMB/UTC_date=20231030/"  
+    #df_trc1 = spark.read.option("recursiveFileLookup", "true").option("header", "true").csv(base_path1).repartition(partitionNum)
 
-    String_Column =['tac','current_cell_id','current_dl_earfcn','current_enodeb_id','current_nr_measurement_arfcn','current_nr_measurement_cell','current_nr_measurement_pci','indoor_outdoor_indicator_name','initial_cell_id','initial_dl_earfcn','initial_enodeb_id','nr_serving_arfcn','nr_serving_pci','nr_sgnb_x2_release_cause_code','service_type_name','serving_nr_cell_id','session_data_lost_flag','subscriber_id','userequipment_imeisv', 'vendor_name', 'userequipment_manufacturer_name', 'userequipment_model_name', 'submkt']
-
-    Date_Column = ['call_end_timestamp','call_start_timestamp','current_nr_rrc_report_time']
-
-    '''
-    df_trc = spark.read.parquet('hdfs://njbbvmaspd11.nss.vzwnet.com:9000/user/ZheS/TrueCall/df_trc_sampled')
-    df_trc_sampled.coalesce(1)
-    df_trc_sampled.cache()
-    df_trc_sampled.count()
-    '''
-
-    base_path1 = "hdfs://njbbvmaspd11.nss.vzwnet.com:9000/user/jennifer/truecall/TrueCall_VMB/UTC_date=20231106/UTC_hour=00/"  
-    df_trc = spark.read.option("recursiveFileLookup", "true").option("header", "true").csv(base_path1).repartition(partitionNum)
-    df_trc.show(1)
-    '''
     today_str = datetime.now().strftime("%Y-%m-%d")
     d_range = get_date_window(today_str, days = 9, direction = 'backward')
     d_range = list( map(lambda x: x.replace("-",""),d_range) )
 
     file_path_pattern = "/user/jennifer/truecall/TrueCall_VMB/UTC_date={}/"  
     df_list = process_csv_files(d_range, file_path_pattern,partitionNum)
+    # def process_csv_files(date_range, file_path_pattern,partitionNum, hdfs = 'hdfs://njbbvmaspd11.nss.vzwnet.com:9000', dropduplicate = False): 
     df_trc = union_df_list(df_list)
-    
-    #print("total number of data", df_trc.count())
-    '''
+    #print(df_trc.columns)
+    print("total number of data", df_trc.count())
 
-
-    # Pre-process----------------------------------------------------------------------
-    
-    df_trc = convert_string_numerical(df_trc,String_Column+Date_Column)
-    df_trc = df_trc.filter(col('end_latitude').isNotNull() ).filter(col('end_longitude').isNotNull() )
-    df_trc = df_trc.withColumn("env_tag", when(col("indoor_outdoor_indicator_name") == "Indoor", "Indoor").otherwise("Outdoor"))
-    #----------------------------------------------------------------------
-
-    # user-defined function to create mgrs/gridid
-    import mgrs
-    m = mgrs.MGRS()
-
-    def convert_to_mgrs(latitude, longitude, MGRSPrecision =3):
-        try:
-            mgrs_value = m.toMGRS(latitude, longitude,MGRSPrecision =MGRSPrecision)
-            return mgrs_value
-        except:
-            return None
-
-    mgrs_udf = udf(convert_to_mgrs, StringType() ) 
-
-    df_trc_mgrs = df_trc.withColumn('gridid', mgrs_udf(df_trc['end_latitude'],df_trc['end_longitude']))
-    df_trc_mgrs.show(1)
-    df_mgrs_feature = df_trc_mgrs.groupBy("gridid", "env_tag").agg(
-                avg("rsrp_dbm").alias("rsrp_dbm"),
-                avg("rsrq_db").alias("rsrq_db"),
-                #avg("ul_sinr_db").alas("ul_sinr_db"), this is an empty column
-                sum("rlc_pdu_dl_bytes_count").alias("sum_rlc_pdu_dl_bytes_count"),
-                sum("rlc_pdu_ul_bytes_count").alias( "sum_rlc_pdu_ul_bytes_count"),
-                sum("dl_mac_volume_bytes").alias( "dl_mac_volume_bytes"),\
-                sum("ul_mac_volume_bytes").alias( "ul_mac_volume_bytes"),\
-                avg("mac_mean_ul_kbps").alias( "mac_mean_ul_kbps"),\
-                avg("mac_mean_dl_kbps").alias( "mac_mean_dl_kbps"),\
-                avg("drb_mean_pdcp_dl_kbps").alias( "drb_mean_pdcp_dl_kbps"),\
-                avg("drb_mean_pdcp_ul_kbps").alias( "drb_mean_pdcp_ul_kbps"),\
-                sum("dl_pdcp_volume_bytes").alias( "dl_pdcp_volume_bytes"),\
-                avg("pusch_sinr_db").alias( "pusch_sinr_db") )
-    df_mgrs_feature.show(1)
-    output_path = 'hdfs://njbbvmaspd11.nss.vzwnet.com:9000/user/ZheS/TrueCall/df_mgrs_feature.csv' 
-
-    df_mgrs_feature.coalesce(100) .write.format("csv").option("header", "true")\
-                    .option("compression", "gzip")\
-                    .mode("overwrite")\
-                    .save(output_path)
-    print('flag')
     # 49193997505 UTC_date=20231031 
     # 49193997505 UTC_date=20231030 
     # 266789049020 UTC_date=20231027-20231031
+    # 504985439804 UTC_date=20231023-20231031
+    """
 
-
+    """
+    base_path1 = "hdfs://njbbvmaspd11.nss.vzwnet.com:9000/user/jennifer/truecall/TrueCall_VMB/UTC_date=20231106/"
+    df_trc_sampled = spark.read.option("recursiveFileLookup", "true").option("header", "true").csv(base_path1).sample(sample_perc).repartition(partitionNum)
+    """
