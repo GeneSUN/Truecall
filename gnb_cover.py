8d4d9d32fc0bab6f1a826c52c29f8e2c15e0eb3c
Zhe Sun <injure21@gmail.com>
2024-01-17 14:58:32 -0500

    Update gnb_cover.py

--- gnb_cover.py
+++ gnb_cover.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, date
-
+from functools import reduce
 from pyspark.sql.window import Window
 from pyspark.sql.functions import row_number, rank, dense_rank, sum, concat_ws, col, split, concat_ws, lit ,udf,count, max,lit,avg, when,concat_ws,to_date,explode
 
@@ -16,7 +16,7 @@ def convert_to_mgrs(latitude, longitude, MGRSPrecision =3):
     except:
         return None
 
-def process_csv_files(date_range, file_path_pattern,sample_percentage = 1, dropduplicate = False): 
+def process_csv_files(date_range, file_path_pattern,partitionNum,sample_percentage = 1, dropduplicate = False): 
 
     """ 
     Reads CSV files from HDFS for the given date range and file path pattern and processes them.
@@ -32,7 +32,9 @@ def process_csv_files(date_range, file_path_pattern,sample_percentage = 1, dropd
         file_path = file_path_pattern.format(d) 
         try:
             if sample_percentage == 1:
-                df_kpis = spark.read.option("recursiveFileLookup", "true").option("header", "true").csv(file_path)
+                df_kpis = spark.read.option("recursiveFileLookup", "true").option("header", "true").csv(file_path)\
+                                .filter(col("serving_nr_cell_id").isNotNull())\
+                                .repartition(partitionNum)
             else:
                 df_kpis = spark.read.option("recursiveFileLookup", "true").option("header", "true").csv(file_path).sample(sample_percentage)
             if dropduplicate:
@@ -45,38 +47,6 @@ def process_csv_files(date_range, file_path_pattern,sample_percentage = 1, dropd
             print(f"data missing at {file_path}")
     return df_list 
 
-def union_df_list(df_list,partitionNum=12000):   
-
-    """   
-    Union a list of DataFrames and apply filters to remove duplicates and null values in 'ENODEB' column.   
-    Args:   
-        df_list (list): List of PySpark DataFrames to be unioned.  
-    Returns:  
-        DataFrame: Unioned DataFrame with duplicates removed and filters applied.   
-
-    """   
-    # Initialize the result DataFrame with the first DataFrame in the list
-    try:
-        df_post = df_list[0]
-    except Exception as e:    
-        # Handle the case where data is missing for the current DataFrame (df_temp_kpi)   
-        print(f"Error processing DataFrame {0}: {e}")   
-        
-    # Iterate through the rest of the DataFrames in the list   
-    for i in range(1, len(df_list)):    
-        try:    
-            # Get the current DataFrame from the list   
-            df_temp_kpi = df_list[i]   
-            # Union the data from df_temp_kpi with df_kpis and apply filters    
-            df_post = (   
-                df_post.union(df_temp_kpi)   
-            )   
-        except Exception as e:    
-            # Handle the case where data is missing for the current DataFrame (df_temp_kpi)   
-            print(f"Error processing DataFrame {i}: {e}")   
-            # Optionally, you can log the error for further investigation   
-    return df_post.repartition(partitionNum)
-
 class gnb_cover():
     global rank_num
     rank_num = 10
@@ -87,7 +57,7 @@ class gnb_cover():
             date_str
         ) -> None:
         self.spark = sparksession
-        self.df_trc = df.filter(col("serving_nr_cell_id").isNotNull()) 
+        self.df_trc = df
         self.mgrs_fun = mgrs_fun
         self.date_str = date_str
         self.df_gnb_mgrs = self.df_trc.withColumn('gridid', 
@@ -136,15 +106,15 @@ if __name__ == "__main__":
         .getOrCreate()
     hdfs_pd = 'hdfs://njbbvmaspd11.nss.vzwnet.com:9000'
     #-----------------------------------------------------------------------
-    partitionNum = 12000; repartion_num = 1000
+    partitionNum = 1000; repartion_num = 1000
 
     start_date = datetime(2023, 12, 17); end_date = start_date + timedelta(6)
-    #start_date = date.today() - timedelta(7) ; end_date = date.today() - timedelta(1)
+    #last_monday = date.today() - timedelta(days=(date.today().weekday() + 6) % 7 + 7);last_sunday = last_monday + timedelta(days=6) 
     d_range = [ (start_date + timedelta(days=x)).strftime('%Y%m%d') for x in range((end_date - start_date).days + 1)] 
 
     file_path_pattern = hdfs_pd + "/user/jennifer/truecall/TrueCall_VMB/UTC_date={}/"  
-    df_list = process_csv_files(d_range, file_path_pattern)
-    df_trc_sampled = union_df_list(df_list,partitionNum)
+    df_list = process_csv_files(d_range, file_path_pattern,partitionNum)
+    df_trc_sampled = reduce(lambda df1, df2: df1.union(df2), df_list)  
 
     mgrs_udf_100 = udf(lambda lat, lon: convert_to_mgrs(lat, lon, MGRSPrecision=3), StringType()) 
     mgrs_udf_1000 = udf(lambda lat, lon: convert_to_mgrs(lat, lon, MGRSPrecision=2), StringType())
@@ -160,26 +130,16 @@ if __name__ == "__main__":
             mgrs_fun=mgrs_udf, 
             date_str=date_range[-1] 
         )
-        #ins.union_df.show()
         ins.union_df.repartition( int(repartion_num*50/precision) )\
             .write.format("csv").option("header", "true")\
             .mode("overwrite")\
             .save(output_path) 
-        #.option("compression", "gzip")\
         """  
 
         """
 
     #process_gnb_cover(spark, df_trc_sampled, mgrs_udf_100, 100, d_range) 
-    #process_gnb_cover(spark, df_trc_sampled, mgrs_udf_1000, 1000, d_range) 
-    process_gnb_cover(spark, df_trc_sampled, mgrs_udf_10000, 10000, d_range) 
+    process_gnb_cover(spark, df_trc_sampled, mgrs_udf_1000, 1000, d_range) 
+    #process_gnb_cover(spark, df_trc_sampled, mgrs_udf_10000, 10000, d_range) 
  
-    """
-    ins100 = gnb_cover(
-                    sparksession = spark,
-                    df = df_trc_sampled,
-                    mgrs_fun = mgrs_udf_100,
-                    date_str = d_range[0]
-                    )
-    """
     #-----------------------------------------------------------------------
